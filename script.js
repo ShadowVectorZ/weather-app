3924ecac13670ed95137757368e420bc7a0597f9
ShadowVectorZ <rayaudet19@gmail.com>
2024-11-12 14:11:12 -0500

    created functions that receive object from API and console.log relevant info

---
+++ script.js
@@ -0,0 +1,15 @@
+let container=document.querySelector('#container') 
+ 
+ fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=9VW6V5TZUV62HFKHVM5C6RFHZ', {mode: 'cors'})
+ .then(function(response) {
+  return response.json();
+})
+  .then(function(response) {
+    console.log(response.currentConditions.conditions);
+    console.log(response.currentConditions.humidity);
+    console.log(response.currentConditions.temp);
+    console.log(response.currentConditions.cloudcover);
+    console.log(response.currentConditions.precip);
+    
+  });
+

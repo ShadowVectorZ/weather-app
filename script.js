93ac533faa353058cc4ef576fbeced8bb181cf11
ShadowVectorZ <rayaudet19@gmail.com>
2024-11-13 10:58:12 -0500

    added code that takes new place and plugs it into the search

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ let container=document.querySelector('#container')
     
   });
 
+  let locationForm = document.querySelector('#locationForm')
+  locationForm.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    let newPlace=document.querySelector('#formLocation').value
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${newPlace}?key=9VW6V5TZUV62HFKHVM5C6RFHZ`, {mode: 'cors'})
+    .then(function(response) {
+     return response.json();
+   })
+     .then(function(response) {
+       console.log(response.currentConditions.conditions);
+       console.log(response.currentConditions.humidity);
+       console.log(response.currentConditions.temp);
+       console.log(response.currentConditions.cloudcover);
+       console.log(response.currentConditions.precip);
+       
+     });
+  }
+  )
+
